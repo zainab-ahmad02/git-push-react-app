dc31c477ba79affe14b388ca02a12082881acd12
zainab-ahmad02 <zainabahmad.edu@gmail.com>
2026-03-06 16:37:05 +0500

    done

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
- base: "/git-push-react-app/",
   plugins: [react()],
-  deploy: "gh-pages -d dist "
 })
